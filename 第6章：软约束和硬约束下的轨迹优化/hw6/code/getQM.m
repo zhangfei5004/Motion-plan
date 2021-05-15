db507993b9c0bae9b4e3ae6814f745f7f1a4aacd
teamo1998 <18810521109@163.com>
2021-05-15 18:48:00 +0300

    更正矩阵系数

--- 第6章：软约束和硬约束下的轨迹优化/hw6/code/getQM.m
+++ 第6章：软约束和硬约束下的轨迹优化/hw6/code/getQM.m
@@ -14,7 +14,7 @@ function [Q, M] = getQM(n_seg, n_order, ts)
             for j = d_order:n_order
                 Q_k(i+1,j+1) = factorial(i)/factorial(i-d_order)*...
                     factorial(j)/factorial(j-d_order)/...
-                    (i+j-n_order)*t_k^(i+j-n_order)/s_k^(2*d_order - 1);
+                    (i+j-n_order)*t_k^(i+j-n_order)/s_k^(2*d_order - 3);
             end
         end
         Q = blkdiag(Q, Q_k);
